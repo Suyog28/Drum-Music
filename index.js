ee812ce6ca5bcbab2572e58ebbcca3c4004165cb
Suyog28 <suyogvmuley@gmail.com>
2023-01-07 22:49:09 +0530

    Updated DrumCode

---
+++ index.js
@@ -0,0 +1,53 @@
+
+let buttonlength = document.querySelectorAll(".drum").length;
+
+for (let i = 0; i < buttonlength; i++) {
+    let drumBtn = document.querySelectorAll("button")[i].addEventListener("click", handleCLick);
+    function handleCLick() {
+
+        let buttonName = this.innerHTML;
+
+        makeSound(buttonName);
+
+    }
+}
+
+document.addEventListener("keydown", function (event) {
+    makeSound(event.key)
+})
+
+function makeSound(key) {
+    switch (key) {
+        case "w":
+            let tom1 = new Audio("./sounds/tom-1.mp3");
+            tom1.play();
+            break;
+        case "a":
+            let tom2 = new Audio("./sounds/tom-2.mp3");
+            tom2.play();
+            break;
+        case "s":
+            let tom3 = new Audio("./sounds/tom-3.mp3");
+            tom3.play();
+            break;
+        case "d":
+            let tom4 = new Audio("./sounds/tom-4.mp3");
+            tom4.play();
+            break;
+        case "j":
+            let crash = new Audio("./sounds/crash.mp3");
+            crash.play();
+            break;
+        case "k":
+            let kick = new Audio("./sounds/kick-bass.mp3");
+            kick.play();
+            break;
+        case "l":
+            let snare = new Audio("./sounds/snare.mp3");
+            snare.play();
+            break;
+
+        default: console.log(buttonName);
+            break;
+    }
+}
